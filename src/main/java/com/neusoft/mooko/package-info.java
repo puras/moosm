92059d05290ed73670c7db8f7fb767d9373dc00d
puras <puras.he@gmail.com>
2015-05-15 16:58:18 +0800

    first commit

---
+++ src/main/java/com/neusoft/mooko/package-info.java
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * @(#)package-info.java 2013年8月26日
+ *
+ * Copyright 2013 Neusoft Group Ltd. All rights reserved.
+ * Neusoft PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *******************************************************************************/
+/**
+ * 
+ * 项目基础包
+ * 
+ * @author <a href="mailto:hegq@neusoft.com">Puras.He</a>
+ * @version $Revision 1.0 $ 2013年8月26日 上午10:35:25
+ */
+package com.neusoft.mooko;
